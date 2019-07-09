de12f4d2aa8b083ff1e6c47ec6f545bc36d33457
César Bolívar Severino <silver.cobra1n@gmail.com>
2019-07-09 15:37:38 -0400

    Modulo para testear el solver.

---
+++ solver.py
@@ -0,0 +1,19 @@
+import solver.solver as Solver
+
+def main():
+    state = 'URDDULBBLUBBFRLFURLRFFFRFUDDLRFDRUDBLFDBLUFLLRDBDBUUBR'
+    face = 'URFDLB'
+    print(state)
+    for f in range(6):
+        print face[f] + ':'
+        for i in range(3):
+            for j in range(3):
+                print state[9*f + 3*i + j],
+            print ''
+        print ''
+    moves = Solver.solve(state, 20, 2)
+    print(moves)
+
+
+if __name__ == '__main__':
+    main()
