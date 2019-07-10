718606f9f7fc73632f1a3936be21bec631eb8231
César Bolívar Severino <silver.cobra1n@gmail.com>
2019-07-10 15:14:16 -0400

    Integrar clasificador GMM a la pipeline.

--- classifier.py
+++ classifier.py
@@ -3,6 +3,9 @@ from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 from mpl_toolkits.mplot3d import Axes3D
 import pylab as pl
+import matplotlib as mpl
+import cv2
+from GMM import GMM
 
 class Classifier(object):
     """docstring for Classifier."""
@@ -25,36 +28,116 @@ class Classifier(object):
     def fit(self, raw_colors):
         self._X = [raw_colors[i] for i in Classifier.FACELET_ORDER]
 
-
+        np.save('X', self._X)
         for label, color in enumerate(self._X):
             color[0], color[2] = color[2], color[0]
-            self._axis.scatter(*color, color=color/255)
+            self._axis.scatter(*color, color=color/384)
         pl.show()
 
         self._X_pca = PCA(n_components=2).fit_transform(self._X)
         print(self._X_pca)
+        np.save('pca', self._X_pca)
         for i, point in enumerate(self._X_pca):
-            pl.scatter(*point, color=self._X[i]/255)
+            pl.scatter(*point, color=self._X[i]/384)
         pl.show()
 
         centroids = np.array([self._X_pca[i] for i in range(4, 54, 9)])
         for i, point in enumerate(centroids):
-            pl.scatter(*point, color=self._X[9*i+4]/255)
+            pl.scatter(*point, color=self._X[9*i+4]/384)
         pl.show()
 
         print(centroids)
         self._model = KMeans(n_clusters=6, init=centroids)
+        # self._model = GaussianMixture(n_components=6, means_init=centroids)
         self._model.fit(self._X_pca)
+
+    def fit2(self, raw_colors):
+        from sklearn.mixture import GaussianMixture
+        self._X = [raw_colors[i] for i in Classifier.FACELET_ORDER]
+        for label, color in enumerate(self._X):
+            color[0], color[2] = color[2], color[0]
+            self._axis.scatter(*color, color=color/384)
+        pl.show()
+
+        centroids = np.array([self._X[i] for i in range(4, 54, 9)])
+
+        print(centroids)
+        self._model = GaussianMixture(n_components=6, means_init=centroids)
+        self._model.fit(self._X)
+        self._X_pca = self._X
+
+
+    def fit3(self, raw_colors):
+        self._X = [raw_colors[i] for i in Classifier.FACELET_ORDER]
+        np.save('X', self._X)
+        X = np.load('X.npy').astype('uint8')
+        for i in range(len(X)):
+            X[i][0], X[i][2] = X[i][2], X[i][0]
+
+        X_hsv = X[np.newaxis, ...]
+        hsv = cv2.cvtColor(X_hsv, cv2.COLOR_RGB2HSV)[0]
+        centroid_id = list(range(4, 54, 9))
+        feats = hsv[:,::2]
+        alphas = np.empty((6, 1))
+        means = np.empty((6, feats.shape[1]))
+        covs = np.empty((6, feats.shape[1], feats.shape[1]))
+        for i in range(6):
+            mean = feats[9*i + 4]
+            cov = np.array([[0.3*50, 0.0], [0.0, 5.0*50]])
+            alphas[i] = 1 / 6.0
+            means[i] = mean
+            covs[i] = cov
+            print('alpha:', alphas[i])
+            print('mean:', mean)
+            print('cov:', cov)
+        self._gmm = GMM(6, feats, mu=means, sigma=covs, alpha=alphas)
+        self._gmm.execute()
+        print(self._gmm.alpha)
+        print(self._gmm.mu)
+        print(self._gmm.sigma)
+
+        self._ans = []
+        for xi in feats:
+            probas = np.array([self._gmm.Normal(xi, self._gmm.mu[k], self._gmm.sigma[k], len(xi)) for k in range(6)])
+            self._ans.append(probas.argmax())
+        print(np.array(self._ans).reshape(6, 3, 3))
+
+        def eigsorted(cov):
+            vals, vecs = np.linalg.eigh(cov)
+            order = vals.argsort()[::-1]
+            return vals[order], vecs[:,order]
+
+        nstd = 2
+        ax = pl.subplot(111)
+        for i, c in enumerate(self._ans):
+            pl.scatter(*feats[i], color=X[centroid_id[c]]/255.0, marker='o')
+
+        for k in range(6):
+            cov = self._gmm.sigma[k]
+            vals, vecs = eigsorted(cov)
+            theta = np.degrees(np.arctan2(*vecs[:,0][::-1]))
+            w, h = 2 * nstd * np.sqrt(vals)
+            ell = mpl.patches.Ellipse(xy=self._gmm.mu[k],
+                          width=w, height=h,
+                          angle=theta, color=X[centroid_id[k]]/255.0, alpha=0.5)
+            ell.set_facecolor(X[centroid_id[k]]/255.0)
+            ax.add_artist(ell)
+        pl.show()
+
 
 
     def get_state(self):
-        if self._X is None:
-            raise Error('Se debe hacer fit primero.')
-        pred2 = self._model.predict(self._X_pca)
-        state = ''.join([self._face_order[i] for i in pred2])
+        # if self._X is None:
+        #     raise Error('Se debe hacer fit primero.')
+        # pred2 = self._model.predict(self._X_pca)
+        # state = ''.join([self._face_order[i] for i in pred2])
+        # print(state)
+        # pred2 = pred2.reshape(-1, 3, 3)
+        # print(pred2)
+        # return state
+        FACES = 'URFDLB'
+        state = ''.join([FACES[i] for i in np.array(self._ans).reshape(-1)])
         print(state)
-        pred2 = pred2.reshape(-1, 3, 3)
-        print(pred2)
         return state
 
 
@@ -62,9 +145,10 @@ class Classifier(object):
 def main():
     reps = np.load('capturas/reps.npy')
     cls = Classifier()
-    cls.fit(reps)
-    ans = cls.get_state()
-    print(ans)
+    # cls.fit(reps)
+    cls.fit3(reps)
+    state = cls.get_state()
+    print(state)
 
 
 if __name__ == '__main__':
--- hsv.py
+++ hsv.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pylab as pl
 import cv2
+import sys
 from mpl_toolkits.mplot3d import Axes3D
 from sklearn.cluster import KMeans, MeanShift
 # from sklearn.mixture import GaussianMixture
@@ -66,9 +67,12 @@ covs = np.empty((6, feats.shape[1], feats.shape[1]))
 for i in range(6):
     f = feats[Y == i]
     print(len(f))
-    mean = f.mean(axis=0)
-    cov = np.cov(f, rowvar=False)
-    alphas[i] = float(len(f)) / len(feats)
+    # mean = f.mean(axis=0)
+    mean = feats[9*i + 4]
+    # cov = np.cov(f, rowvar=False)
+    cov = np.array([[0.3*50, 0.0], [0.0, 5.0*50]])
+    # alphas[i] = float(len(f)) / len(feats)
+    alphas[i] = 1 / 6.0
     means[i] = mean
     covs[i] = cov
     print('alpha:', alphas[i])
@@ -88,6 +92,7 @@ print("The likelihood is:", gmm.likelihood)
 print("The amplitudes are:", gmm.alpha)
 print("The means are:", gmm.mu)
 print("The covariances are:", gmm.sigma)
+# sys.exit()
 
 ans = []
 for xi in feats:
@@ -98,7 +103,7 @@ for xi in feats:
 print(np.array(ans).reshape(-1, 3, 3))
 
 for i, c in enumerate(ans):
-    pl.scatter(*feats[i], color=X[centroid[c]]/255.0, marker='.')
+    pl.scatter(*feats[i], color=X[centroid[c]]/255.0, marker='o')
 # pl.show()
 
 
@@ -114,6 +119,7 @@ ax = pl.subplot(111)
 
 for k in range(6):
     cov = gmm_sigma[k]
+    # cov = np.array([[0.3*50, 0.0], [0.0, 5.0*50]])
     vals, vecs = eigsorted(cov)
     theta = np.degrees(np.arctan2(*vecs[:,0][::-1]))
     w, h = 2 * nstd * np.sqrt(vals)
